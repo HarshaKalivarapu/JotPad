50c597c9f9d3d4e850302e8175c77e386992b3f2
Harsha Kalivarapu <harshavk27@gmail.com>
2026-05-24 16:41:36 -0400

    wired page, sidebar, and topbar together

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
 import './index.css'
+import { useState } from 'react'
+import TopBar from './components/TopBar.jsx'
+import SideBar from './components/SideBar.jsx'
+import Page from './components/Page.jsx'
 
 function App() {
+  const [sideBar, setSideBar] = useState(false)
+
+  function handleSideBarToggle() {
+    setSideBar(!sideBar)
+  }
+
   return (
     <div className="app-container">
+      <TopBar switchToggle={handleSideBarToggle}/>
+      <div className="main-container">
+        {sideBar && <SideBar />}
+        <Page />
+      </div>
       <h1>JotPad is alive.</h1>
     </div>
   )
