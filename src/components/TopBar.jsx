184e4a56bc58995c27364ebadecde1405058a3f7
Harsha Kalivarapu <harshavk27@gmail.com>
2026-05-28 19:45:24 -0400

    updated formatting for side panel toggle button

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -3,7 +3,7 @@ import { PanelRightClose } from 'lucide-react'
 function TopBar(props) {
     return (
         <div className="top-bar-panel">
-            <button onClick={props.switchToggle}><PanelRightClose /></button>
+            <button className="icon-button" onClick={props.switchToggle}><PanelRightClose size={20} /></button>
             <h2>{props.currentPage?.title || "Loading..."}</h2>
             <button>Settings</button>
         </div>
